fbc97654803d1302d2e8f95230d598f22db629e7
Carles95973 <carles.farre.serra@gmail.com>
2018-11-18 19:52:21 +0100

    Add css, mod html and mod settings configuration static files

--- AECOlink/AECOlink/settings.py
+++ AECOlink/AECOlink/settings.py
@@ -119,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
@@ -134,3 +134,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    ]
+
+STATICFILES_FINDERS = [
+
+]
