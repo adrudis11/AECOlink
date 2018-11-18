3eecf75bd4119563a0ff54ca92b7a1a871b1a87b
Carles95973 <carles.farre.serra@gmail.com>
2018-11-18 20:43:43 +0100

    Nose que e fet

--- AECOlink/AECOlink/settings.py
+++ AECOlink/AECOlink/settings.py
@@ -134,12 +134,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'static', 'media')
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static', 'static_onli')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    'AECOnews/static',
     ]
-
-STATICFILES_FINDERS = [
-
-]
