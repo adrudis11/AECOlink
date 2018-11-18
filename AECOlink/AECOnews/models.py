2549106bc68106eaf6a96a3d9480d8ac6a849f4d
Carles95973 <carles.farre.serra@gmail.com>
2018-11-18 12:52:14 +0100

    Add class Post and mod htmls

--- AECOlink/AECOnews/models.py
+++ AECOlink/AECOnews/models.py
@@ -5,3 +5,10 @@ class User(models.Model):
 
 class Client(User):
     asdf = models.CharField(max_length=50)
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    image = models.ImageField(upload_to='post_image', null=True)
+    create_at = models.DateTimeField(auto_now=True, auto_now_add=False, null=True)
+    update_at = models.DateTimeField(auto_now=False, auto_now_add=True, null=True)
