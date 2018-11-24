618f7bc3d3bd4a4ef949495631f3d4dd763abf64
Albert Drudis <albert.drudis@gmail.com>
2018-11-24 17:46:30 +0100

    Post creation

--- AECOlink/apps/AECOnews/views.py
+++ AECOlink/apps/AECOnews/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    post = Post(title="Title", body="Text", image="Image");
+    post.save(force_insert=True)
+    return render(request, 'index.html', { "post" : post })
