cb86db24db4cb58fcf2f4d0a84a2958455f2cbeb
Albert Drudis <albert.drudis@gmail.com>
2018-11-24 18:20:58 +0100

    Post box created

--- AECOlink/apps/AECOnews/models.py
+++ AECOlink/apps/AECOnews/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from AECOlink.apps.profiles.models import Client
 
 class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=150)
     body = models.TextField()
     image = models.ImageField(upload_to='post_image', null=True)
     create_at = models.DateTimeField(auto_now=True, auto_now_add=False, null=True)
     update_at = models.DateTimeField(auto_now=False, auto_now_add=True, null=True)
+
 
--- AECOlink/apps/AECOnews/views.py
+++ AECOlink/apps/AECOnews/views.py
@@ -4,6 +4,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    post = Post(title="Title", body="Text", image="Image");
-    post.save(force_insert=True)
+    post = Post.objects.get(title="Title")
     return render(request, 'index.html', { "post" : post })
