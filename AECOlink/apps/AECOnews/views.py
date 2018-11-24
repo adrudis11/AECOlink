9994f1f4b0e386d0fa8387edad32677ec10323c3
Albert Drudis <albert.drudis@gmail.com>
2018-11-24 20:16:19 +0100

    Model instanced and passed to front

--- AECOlink/apps/AECOnews/views.py
+++ AECOlink/apps/AECOnews/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    post = Post(title="Title", body="Text", image="img.png")
-    #post = Post.objects.get(title="Title")
+    #post = Post(title="Title", body="Text", image="img.png")
+    #post.save()
+    post = Post.objects.get(title="Title")
     return render(request, 'index.html', { "post" : post })
